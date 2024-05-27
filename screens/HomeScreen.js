72029c67a31e44aac7a6dfad060db3bfe2fa30db
MontherTuwati <monthertuwati@gmail.com>
2024-05-27 11:46:13 -0400

    added heroicons and searchbear

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,8 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TextInput } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { AdjustmentsVerticalIcon, ChevronDownIcon, MagnifyingGlassIcon, UserIcon } from 'react-native-heroicons/outline';
 
 const HomeScreen = () => {
     const navigation = useNavigation();
@@ -13,8 +14,8 @@ const HomeScreen = () => {
     }, [])
 
   return (
-    <SafeAreaView>
-      <Text className='text-red-500'>
+    <SafeAreaView className="bg-white pt-5">
+      <View className='text-red-500'>
 
         {/*Header*/}
         <View className="flex-row pb-3 items-center mx-4 space-x-2">
@@ -24,13 +25,29 @@ const HomeScreen = () => {
                 }}
                 className="h-7 w-7 bg-gray-300 p-4 rounded-full"
             />
-            <View>
+            
+            <View className="flex-1">
                 <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-                <Text className="font-bold text-xl">Current Location</Text>
+                <Text className="font-bold text-xl">Current Location
+                  <ChevronDownIcon size={20} color="#00CCBB"/>
+                </Text>
             </View>
+
+            <UserIcon size={35} color="#00CCBB"/>
         </View>
-                
-      </Text>
+        
+        {/*Search Bar*/}
+        <View className="flex-row items-center space-x-2 pb-2 mx-4">
+          <View className="flex-row items-center space-x-2 flex-1 bg-gray-200 p-3">
+            <MagnifyingGlassIcon color='gray' size={20}/>
+            <TextInput
+              placeholder="Restaurants and cuisines" 
+              keyboardType="default"
+            />
+          </View>
+          <AdjustmentsVerticalIcon color='#00CCBB'/>
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
